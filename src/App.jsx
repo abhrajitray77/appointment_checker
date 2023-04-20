947b07235fe15403615697c260abd10705bcdbeb
abhrajitray77 <abhrajitray77@gmail.com>
2023-04-20 18:20:14 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Banner from "./components/Banner";
+import Datepick from "./components/Datepick";
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <div className="p-8">
+          <h1 className="text-5xl font-semibold">Appointment Checker</h1>
+        </div>
+      </nav>
+      <Banner />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Banner.jsx
@@ -0,0 +1,14 @@
+
+import docpic from "../assets/docmain.webp"
+
+const Banner = () => {
+  return (
+    <div className=''>
+        <div className='p-20'>
+        <img src={docpic} alt="docpic" className='w-[400px] rounded-3xl' />
+        </div>
+    </div>
+  )
+}
+
+export default Banner
